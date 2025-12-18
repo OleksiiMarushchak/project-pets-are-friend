73d983d6589325c9c0502adee2023086d9b4685c
Olha Brekhuntsova <olga.pistryak@gmail.com>
2025-12-18 23:16:28 +0100

    add phone input formatting

--- src/javaScript/sweetAlert.js
+++ src/javaScript/sweetAlert.js
@@ -11,7 +11,7 @@ const notification = {
   successAllert(message) {
     '';
     Swal.fire({
-      title: 'Відправка успішна!',
+      title: 'Ми отримали вашого листа!',
       icon: 'success',
       text: message,
     });
